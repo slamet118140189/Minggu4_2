dec829548c24c592e7a9244b63b05a5ba6456b21
slamet118140189 <80862997+slamet118140189@users.noreply.github.com>
2022-11-11 14:39:14 +0700

    Add files via upload
    Work

---
+++ mode.js
@@ -0,0 +1,10 @@
+function darkmode() {
+  var element = document.body;
+  var content = document.getElementById("container")
+  element.className = "dark-mode";
+}
+function lightmode() {
+  var element = document.body;
+  var content = document.getElementById("container");
+  element.className = "light-mode";
+}
